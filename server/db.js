bc3b48c29d863034e626406e287dd1ffd5b072bf
Santeri Suomi <sant.suomi@gmail.com>
2021-12-12 01:06:32 +0200

    test

--- server/db.js
+++ server/db.js
@@ -4,27 +4,35 @@ require("dotenv").config();
 class DB {
 	constructor() {
 		this.pool = mysql.createPool({
-			connectionLimit: 15,
 			host: process.env.host,
 			user: process.env.user,
 			password: process.env.pass,
 			database: process.env.db,
+			connectionLimit: 15,
 		});
 	}
 
 	getAll() {
 		return new Promise((resolve, reject) => {
-			this.pool.getConnection((err, connection) => {
-				if (err) reject(err);
-				connection.query(
-					"SELECT * FROM test",
-					(err, results, fields) => {
-						connection.release();
-						if (err) reject(err);
-						resolve(JSON.parse(JSON.stringify(results)));
-					}
-				);
+			this.pool.query("SELECT * FROM test", (err, results, fields) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					resolve(JSON.parse(JSON.stringify(results)));
+				}
 			});
+			// this.pool.getConnection((err, connection) => {
+			// 	if (err) reject(err);
+			// 	connection.query(
+			// 		"SELECT * FROM test",
+			// 		(err, results, fields) => {
+			// 			connection.release();
+			// 			if (err) reject(err);
+			// 			resolve(JSON.parse(JSON.stringify(results)));
+			// 		}
+			// 	);
+			// });
 		});
 	}
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.listen(PORT, () => {
@@ -14,14 +16,11 @@ app.listen(PORT, () => {
 
 // API
 app.get("/test", async (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	let result = await db.getAll();
-	console.log(result);
-	res.send(result);
+	let response = await db.getAll();
+	res.send(response);
 });
 
 // If API is not accessed, send frontend instead
 app.get("*", (_, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
